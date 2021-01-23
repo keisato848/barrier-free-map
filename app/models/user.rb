40d90c023c1bcd5257a2a30140be20fc66a4b88b
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-01-23 22:10:02 +0900

    バリデーションの設定、エラーメッセージの日本語対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname, presence: true, uniqueness: true
+  validates :profile, length: { maximum: 140 }
 end
